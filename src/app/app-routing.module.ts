0b6054ee13077f5ae718d9df893bb7084a33a025
Pablo Limardo <plimardo@gmail.com>
2022-09-06 22:19:50 -0300

    Subiendo Prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {  path: 'contacto', component: ContactoComponent},
+  {  path: 'galeria', component: GaleriaComponent},
+  {  path: '**', component: PagenotfoundComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GaleriaComponent,
+    ContactoComponent,
+    NavbarComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
